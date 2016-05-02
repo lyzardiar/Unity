5cb8b6b4a585fc7429cabc770106505fac5933d4
MiYu <233140882@qq.com>
2016-05-02 21:36:03 +0800

    add first files ...

---
+++ Core/MonobehaviorEx.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// override class MonoBehaviour
+public class MonoBehaviour : UnityEngine.MonoBehaviour
+{
+	//// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+ //       Debug.Log("Update in MonobehaviorEx, Please Override...");
+	//}
+
+
+}
